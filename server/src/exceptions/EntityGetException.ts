8a3156d1d0cb7a4a31c82b4e57399ae2ed373df8
Dariusz Syncerek <dsyncerek@outlook.com>
2019-06-16 12:24:28 +0200

    Fix exception name

--- server/src/exceptions/EntityGetException.ts
+++ server/src/exceptions/EntityGetException.ts
@@ -1,7 +1,7 @@
 import BaseException from './BaseException';
 
 class EntityGetException extends BaseException {
-  public type = "entity_not_found_exception";
+  public type = "entity_get_exception";
   public status = 404;
 }
 
