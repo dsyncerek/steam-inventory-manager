0b86bc7d76b7954fae1a219a78792139d5f56eca
Dariusz Syncerek <dsyncerek@outlook.com>
2020-01-27 23:16:02 +0100

    Update jest

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverageFrom: ['src/**/*.ts'],
-  coverageReporters: ['text', 'html'],
+  coverageReporters: ['text', 'html-spa'],
 };
