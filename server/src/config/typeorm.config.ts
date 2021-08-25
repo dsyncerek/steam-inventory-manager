a9359abbf23b04e2012ddb59e3868397592b8149
Dariusz Syncerek <dsyncerek@outlook.com>
2021-08-25 19:13:47 +0200

    fix

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -1,15 +1,15 @@
-import { ConnectionOptions, getConnectionOptions } from 'typeorm';
+import { getConnectionOptions } from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { Bot } from '../modules/bot/entity/bot.entity';
 import { InventoryItem } from '../modules/inventory/entity/inventory-item.entity';
 import { Inventory } from '../modules/inventory/entity/inventory.entity';
 import { Item } from '../modules/item/entity/item.entity';
 import { User } from '../modules/user/entity/user.entity';
 
-export async function getTypeOrmConfig(): Promise<ConnectionOptions> {
+export async function getTypeOrmConfig(): Promise<PostgresConnectionOptions> {
   return {
-    ...(await getConnectionOptions()),
+    ...((await getConnectionOptions()) as PostgresConnectionOptions),
     entities: [Bot, Inventory, InventoryItem, User, Item],
-    extra: { ssl: true },
-    ssl: true,
+    ssl: { rejectUnauthorized: false },
   };
 }
