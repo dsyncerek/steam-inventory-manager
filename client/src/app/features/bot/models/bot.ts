73d4f514865585eaa32341f60af0db966b453a36
Dariusz Syncerek <dsyncerek@outlook.com>
2021-10-13 12:49:09 +0200

    Update bot.ts

--- client/src/app/features/bot/models/bot.ts
+++ client/src/app/features/bot/models/bot.ts
@@ -15,4 +15,4 @@ export interface Bot {
 const idAttribute: keyof Bot = 'steamId';
 export const botSchema = new schema.Entity('bots', { inventories: [inventorySchema] }, { idAttribute });
 export const selectBotId = (bot: Bot): string => bot[idAttribute];
-export const botSortComparer = (a: Bot, b: Bot): number => a.login.localeCompare(b.name);
+export const botSortComparer = (a: Bot, b: Bot): number => a.name.localeCompare(b.name);
